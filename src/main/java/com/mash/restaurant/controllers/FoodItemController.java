50e9b48003429d3b474771205245248c26622b05
Roman Arnautov <arnautov.roma2000@gmail.com>
2023-03-20 15:00:26 +0700

    restful api paths

--- src/main/java/com/mash/restaurant/controllers/FoodItemController.java
+++ src/main/java/com/mash/restaurant/controllers/FoodItemController.java
@@ -4,11 +4,13 @@ import com.mash.restaurant.models.FoodItem;
 import com.mash.restaurant.repositories.FoodItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/food")
 public class FoodItemController {
 
     private final FoodItemRepository foodItemRepository;
@@ -18,15 +20,16 @@ public class FoodItemController {
         this.foodItemRepository = foodItemRepository;
     }
 
-    @GetMapping("/food")
+    @GetMapping
     public @ResponseBody Iterable<FoodItem> getAllFoodItems() {
         return foodItemRepository.findAll();
     }
 
-    @PostMapping(path = "/add_food",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<FoodItem> newFoodItem(@RequestBody FoodItem newFoodItem){
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<FoodItem> getFoodItem(@PathVariable Integer id) { return foodItemRepository.findById(id); }
+
+    @PostMapping
+    public ResponseEntity<FoodItem> addFoodItem(@RequestBody FoodItem newFoodItem){
         FoodItem foodItem = foodItemRepository.save(newFoodItem);
         return new ResponseEntity<>(foodItem, HttpStatus.CREATED);
     }
